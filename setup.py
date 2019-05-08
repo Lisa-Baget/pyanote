dcf0ca781d6bec06ed1f5214145473d35aa36f50
Lisa-Baget <li.baget@laposte.net>
2019-05-08 22:05:25 +0200

    Ajout d'un setup pour installer et tenter de regler problemes de noms modules

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyanote',
+   version='0.0.0-beta.2',
+   description='Un projet ISN de manipulation de fichiers MIDI.',
+   author='Lisa Baget',
+   author_email='li.baget@laposte.net',
+   packages=['pyanote'], 
+   install_requires=['pygame'] 
+)
