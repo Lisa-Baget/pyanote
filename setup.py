49c044285ad0d2691dee56d91d684f6ef3b2f700
Lisa-Baget <li.baget@laposte.net>
2019-05-22 15:47:18 +0200

    Nouvelle version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
    name='pyanote',
-   version='0.0.0-beta.4',
+   version='0.0.1-beta.0',
    description='Un projet ISN de manipulation de fichiers MIDI.',
-   author='Lisa Baget',
+   author='Lisa Baget and Matthieu Durand',
    author_email='li.baget@laposte.net',
    packages=['pyanote'], 
    install_requires=['pygame']
