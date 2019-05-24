47597e44ebbd88f1911d04573c15114a18f1e4d9
Lisa-Baget <li.baget@laposte.net>
2019-05-24 11:12:42 +0200

    modification setup pour macos

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from setuptools import setup
+import pyanote
 
 setup(
    name='pyanote',
-   version='0.0.1-beta.0',
+   version='0.0.1-beta.1',
    description='Un projet ISN de manipulation de fichiers MIDI.',
    author='Lisa Baget and Matthieu Durand',
    author_email='li.baget@laposte.net',
